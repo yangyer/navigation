52f07451dc0c8a5630a126f69a2a991974271667
Yer Yang <yer.yang@availcarsharing.com>
2020-11-12 15:44:22 -0800

    update code base on changes in lib

--- src/App.js
+++ src/App.js
@@ -3,19 +3,8 @@ import './App.css';
 const App = ({
   onNavClick
 }) => {
-  const routes = [{
-    title: 'route 1'
-  }]
   return (
     <button onClick={() => onNavClick('from nav bar')}>click me</button>
-    // <div>
-    //   <ul>
-    //     {routes && routes
-    //       .forEach(r => (
-    //         <li onClick={() => onNavClick(`${r.title} was clicked`)}>r.title</li>
-    //       ))}
-    //   </ul>
-    // </div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,57 +2,20 @@ import React from 'react'
 import { render, unmountComponentAtNode } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
-import { createMicroFERegistration } from 'avail-microfe-base'
+import { createMicrofrontendRegistration } from 'avail-microfe-base'
 
-const pubSub = () => {
-    const subscribers = {}
-  
-  function publish(eventName, data) {
-    if (!Array.isArray(subscribers[eventName])) {
-      return
-    }
-    subscribers[eventName].forEach((callback) => {
-      callback(data)
-    })
-  }
-  
-  function subscribe(eventName, callback) {
-    if (!Array.isArray(subscribers[eventName])) {
-      subscribers[eventName] = []
-    }
-    subscribers[eventName].push(callback)
-  }
-  
-  return {
-    publish,
-    subscribe,
-  }
-}
-createMicroFERegistration({
+createMicrofrontendRegistration({
     id: 'navigation',
-    init: (state, containerId, options) => {
-        const { history } = options
-        const listenerPubSub = pubSub()
-        listenerPubSub.subscribe('listener', (msg) => console.log('msg', msg))
+    init: (containerId, options) => {
+        const { history, publish } = options
+        let counter = 0
         render(
-            <BrowserRouter>
-                <App history={history} onNavClick={(msg => {
-                    listenerPubSub.publish('listener', {
-                        type: 'NAVIGATION',
-                        payload: msg
-                    })
-                })} />
-            </BrowserRouter>,
-            document.getElementById(containerId)
-        )
-        return {
-            registerLayer: (layer) => {
-                listenerPubSub.subscribe('listener', (msg) => {
-                    layer.emitter(msg)
-                })
-                return Promise.resolve()
-            }
-        }
+          <BrowserRouter>
+              <App history={history} onNavClick={(msg) => publish('navigation', `${msg}${counter++}`)} />
+          </BrowserRouter>,
+          document.getElementById(containerId)
+      )
+      return Promise.resolve()
     },
     unmount: (containerId) => {
         unmountComponentAtNode(document.getElementById(containerId))
